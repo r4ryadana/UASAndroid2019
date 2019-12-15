630712a7abebeaf7d2faf8c13e4db7df5efd12f7
arikyahya <arikyahya02@gmail.com>
2019-12-15 15:10:25 +0700

    pilihan kuis

---
+++ app/src/main/java/id/ac/polinema/uasandroid2019/activities/HasilSkoring.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.uasandroid2019.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HasilSkoring extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_skoring);
+    }
+}
---
+++ app/src/main/java/id/ac/polinema/uasandroid2019/activities/KuisEssay.java
@@ -0,0 +1,16 @@
+package id.ac.polinema.uasandroid2019.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.ac.polinema.uasandroid2019.R;
+
+public class KuisEssay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kuis_essay);
+    }
+}
